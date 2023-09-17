f5bd759024e69047ce4ffdfec41d53ffeeb7d4fc
clarksydney3 <clarksydney3@gmail.com>
2023-09-17 14:42:30 -0500

    adding these

---
+++ C++ 1570/notes.cpp
@@ -0,0 +1,22 @@
+// Your First C++ Program
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;
+}
+// Definiteness: each stop is precisly stated. instructions are clear and preside with no ambiguity.
+// Effectiveness: Every instructoin must be basic and executable.
+//example starting
+//given a list of numbers identify and return the largest number.
+//create pseudocode
+//begin code:
+// SET largestNumber TO list[0]
+// FOR each number IN list DO
+//     IF number > largestNumber THEN
+//         SET largestNumber TO number
+//     END IF
+// END FOR - ends loop
+// RETURN largestNumber
+//draw a flowchart to visuailze the program,
+//and where the info/data flows and outputted.
